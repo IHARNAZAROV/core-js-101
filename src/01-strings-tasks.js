853ddb15bd41044ffd789a58e14943c2fd5b71cd
Igor Nazarov <grizley.tery@gmail.com>
2020-09-10 16:49:25 +0300

    feat: add some solutions

--- src/01-strings-tasks.js
+++ src/01-strings-tasks.js
@@ -9,33 +9,33 @@
 /**
  * Returns the result of concatenation of two strings.
  *
- * @param {string} value1
- * @param {string} value2
- * @return {string}
+ @param {string} value1
+ @param {string} value2
+ @return {string}
  *
  * @example
  *   'aa', 'bb' => 'aabb'
  *   'aa',''    => 'aa'
  *   '',  'bb'  => 'bb'
  */
-function concatenateStrings(/* value1, value2 */) {
-  throw new Error('Not implemented');
+function concatenateStrings(value1, value2) {
+  return value1.toString() + value2.toString();
 }
 
 
 /**
  * Returns the length of given string.
  *
- * @param {string} value
- * @return {number}
+ @param {string} value
+ @return {number}
  *
  * @example
  *   'aaaaa' => 5
  *   'b'     => 1
  *   ''      => 0
  */
-function getStringLength(/* value */) {
-  throw new Error('Not implemented');
+function getStringLength(value) {
+  return value.length;
 }
 
 /**
@@ -43,16 +43,16 @@ function getStringLength(/* value */) {
  * Please do not use concatenation, use template string :
  * https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/template_strings
  *
- * @param {string} firstName
- * @param {string} lastName
- * @return {string}
+ @param {string} firstName
+ @param {string} lastName
+ @return {string}
  *
  * @example
  *   'John','Doe'      => 'Hello, John Doe!'
  *   'Chuck','Norris'  => 'Hello, Chuck Norris!'
  */
-function getStringFromTemplate(/* firstName, lastName */) {
-  throw new Error('Not implemented');
+function getStringFromTemplate(firstName, lastName) {
+  return `Hello, ${firstName} ${lastName}!`;
 }
 
 /**
